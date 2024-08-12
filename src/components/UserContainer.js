338a314f6164c93abf585d4bd86e0ef1efc3a94c
abdul <abdul.tanveer@gmail.com>
2024-08-12 17:18:47 +0530

    thunk -- async call

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { fetchUsers } from "../redux";
+import { connect } from "react-redux";
 
-export default function UserContainer() {
-  return (
-    <div>UserContainer</div>
-  )
+function UserContainer(userData, fetchUsers) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return userData.loading ? (
+    <h2>loading...</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      {userData &&
+        userData.users &&
+        userData.users.map((user) => <p> {user.name}</p>)}
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => {
+      dispatch(fetchUsers());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
