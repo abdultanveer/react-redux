325c636f5e8dcdb65b6b1496a07e9715ecad2ce9
abdul <abdul.tanveer@gmail.com>
2024-08-12 15:54:11 +0530

    ownprops -- dispatch state

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake, buyIcecream } from "../redux";
 
- function ItemContainer(props) {
+function ItemContainer(ownProps) {
   return (
     <div>
-        <h2>Own Item - {props.item}</h2>
+      <h2>Own Item - {ownProps.item}</h2>
+      <button onClick={ownProps.buyItem}>buy items</button>
     </div>
-  )
+  );
 }
 
-
-const mapStateToProps = (state,ownProps) => {
-    const itemState  = 
-    ownProps.cake ? state.cake.numCakes
-    :state.iceCream.numIcreams
-    return {
-     item: itemState
-  
-    };
+const mapStateToProps = (state, ownProps) => {
+  const itemState = ownProps.cake
+    ? state.cake.numCakes
+    : state.iceCream.numIcreams;
+  return {
+    item: itemState,
   };
+};
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//       buyCake: () => dispatch(buyCake()),
-//       buyIcecream: () => dispatch(buyIcecream()),
-  
-//     };
-//   };
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake(1))
+    : () => dispatch(buyIcecream());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
 
-export default connect(mapStateToProps)(ItemContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
