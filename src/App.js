d17c742ec0d953c245bfbe137a73a699fa266155
abdul <abdul.tanveer@gmail.com>
2024-08-12 12:53:18 +0530

    useCounter custom hook

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import DocTitleOne from "./hooks/DocTitleOne";
 import DocTitleTwo from "./hooks/DocTitleTwo";
+import CounterOne from "./hooks/CounterOne";
+import CounterTwo from "./hooks/CounterTwo";
 //npm i react-redux
 function App() {
   return (
     // <Provider store={store}>
       <div className="App">
-      <DocTitleOne/>
-      <DocTitleTwo/>
+      <CounterOne/>
+      <CounterTwo/>
         {/* <CakeContainer />
         <HooksCakeContainer /> */}
       </div>
